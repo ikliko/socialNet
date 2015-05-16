1f24845f9797d60c09d16fe760f6d07d3928a3bd
ikliko <kliko.atanasov@gmail.com>
2015-05-16 22:10:02 +0300

    add

--- socialnet/scripts/socialNet.js
+++ socialnet/scripts/socialNet.js
@@ -1,3 +1,21 @@
 /**
  * Created by kliko on 15.05.15.
  */
+var socialNet = angular.module('socialNet', ['ngResource', 'ngRoute'])
+    .config(function($routeProvider, $locationProvider) {
+
+        $routeProvider
+            .when('/', {
+                template : '<div>home</div>'
+            })
+            .when('/login', {
+                template : '<div>login</div>'
+            })
+            .when('/register', {
+                template : 'register'
+            });
+
+        // use the HTML5 History API
+        $locationProvider.html5Mode(false)
+            .hashPrefix('!');
+    });
